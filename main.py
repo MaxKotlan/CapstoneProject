25f002e2b58f68e7010d9d2685762556ab5463b0
Max Kotlan <max.maxkot@gmail.com>
2019-10-27 11:47:50 -0400

    moved angular app into subdirectory. Added flask template

---
+++ main.py
@@ -0,0 +1,33 @@
+from flask import Flask
+from flask import send_from_directory
+from werkzeug.routing import BaseConverter
+
+
+app = Flask(__name__, static_url_path="", static_folder="angular/dist/")
+
+class RegexConverter(BaseConverter):
+    def __init__(self, url_map, *items):
+        super(RegexConverter, self).__init__(url_map)
+        self.regex = items[0]
+
+
+app.url_map.converters['regex'] = RegexConverter
+
+# @app.route("/")
+# def hello():
+#     return "Hello World!"
+
+@app.route("/")
+def angular():
+    return send_from_directory("angular/dist", "index.html") 
+
+@app.route("/<regex('\w\.(js|css)'):path>")
+def angular_src(path):
+    return send_from_directory("angular/dist", path)
+
+@app.route("/hello")
+def hello():
+    return "Hello World!"
+
+if __name__ == "__main__":
+    app.run()
