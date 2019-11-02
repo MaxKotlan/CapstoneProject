6cbdfc43799bd50971391953ce14e5c11f551989
Max Kotlan <mkotlan@kent.edu>
2019-11-02 13:09:50 -0400

    cleaned up text-post component. added admin component

--- angular/src/app/components/text-posts/text-posts.component.ts
+++ angular/src/app/components/text-posts/text-posts.component.ts
@@ -1,7 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { DataService } from 'src/app/services/data.service';
 import { Observable, interval } from 'rxjs';
-import { map } from 'rxjs/operators';
 
 import { TextPost } from 'src/app/models/TextPost';
 
@@ -10,21 +9,10 @@ import { TextPost } from 'src/app/models/TextPost';
   templateUrl: './text-posts.component.html',
   styleUrls: ['./text-posts.component.scss']
 })
-export class TextPostsComponent implements OnInit {
+export class TextPostsComponent {
 
   constructor(private dataService : DataService) { }
 
-  posts : Observable<Array<TextPost>>;
-  
-  ngOnInit() {
-    this.posts = this.dataService.getAll();
-    //let j = [];
-    //this.k =  interval(1000).pipe(map(x => {j.push(new TextPost(x, "test", "test", "test")); return j;}));
-    //this.k.subscribe(x => console.log(x));
-  }
-
-  ngOnDestroy(){
-    
-  }
+  posts : Observable<Array<TextPost>> = this.dataService.getAll();
 
 }
--- angular/src/app/components/works/works.component.ts
+++ angular/src/app/components/works/works.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { Observable, interval } from 'rxjs';
+import { TextPost } from 'src/app/models/TextPost';
+import { DataService } from 'src/app/services/data.service';
+import { map } from 'rxjs/operators';
 
 @Component({
   selector: 'app-works',
@@ -7,9 +11,14 @@ import { Component, OnInit } from '@angular/core';
 })
 export class WorksComponent implements OnInit {
 
-  constructor() { }
+  constructor( private dataService : DataService ) { }
+
+  posts : Observable<Array<TextPost>>;// = this.dataService.getAll();
 
   ngOnInit() {
+    let j = [];
+    this.posts =  interval(1000).pipe(map(x => {j.push(new TextPost(x, "work " + x, "test", "test", "test")); return j;}));
+
   }
 
 }
