0bf2c99ea0178a91d69af76b53b4f786c8bf6e74
max <max.maxkot@gmail.com>
2019-11-06 20:41:36 -0500

    added error and success message on adminlogin. final animation speed

--- angular/src/app/components/adminlogin/adminlogin.component.ts
+++ angular/src/app/components/adminlogin/adminlogin.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { LoginService } from 'src/app/services/login.service';
 import { Login } from 'src/app/models/Login';
+import { ToastService } from 'ng-uikit-pro-standard';
+import { HttpErrorResponse } from '@angular/common/http';
 
 @Component({
   selector: 'app-adminlogin',
@@ -9,12 +11,18 @@ import { Login } from 'src/app/models/Login';
 })
 export class AdminloginComponent {
 
-  constructor(private loginService : LoginService) { }
+  constructor(
+    private loginService : LoginService,
+    private toast : ToastService
+  ) { }
 
   public user : Login = new Login();
 
   LogIn(){
-    this.loginService.login(this.user).subscribe(x => console.log(x));
+    this.loginService.login(this.user).toPromise().then(
+      (res : string) => this.toast.success("Succesfully Logged in as " + this.user.username, "Success"),
+      (err : HttpErrorResponse)=> this.toast.error("Could not log in.", "Error")
+    );   
   }
 
 }
