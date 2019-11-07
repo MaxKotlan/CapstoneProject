201b6a25046d906a26550ba9edeb625fb92b070a
max <max.maxkot@gmail.com>
2019-11-07 12:21:32 -0500

    now gives a success message on login page

--- angular/src/app/components/adminlogin/adminlogin.component.ts
+++ angular/src/app/components/adminlogin/adminlogin.component.ts
@@ -3,6 +3,9 @@ import { LoginService } from '../../global/services/login.service';
 import { Login } from '../../global/models/Login';
 import { ToastService } from 'ng-uikit-pro-standard';
 import { HttpErrorResponse } from '@angular/common/http';
+import { Store, select } from '@ngrx/store';
+import { Observable } from 'rxjs';
+import { isLoggedInSuccesfully } from 'src/app/global/actions/login.actions';
 
 @Component({
   selector: 'app-adminlogin',
@@ -13,14 +16,19 @@ export class AdminloginComponent {
 
   constructor(
     private loginService : LoginService,
+    private store: Store<{ isLoggedIn : boolean }>,
     private toast : ToastService
   ) { }
 
+  isLoggedIn$ : Observable<boolean> = this.store.pipe(select('isLoggedIn'));
   public user : Login = new Login();
 
   LogIn(){
     this.loginService.login(this.user).toPromise().then(
-      (res : string) => this.toast.success("Succesfully Logged in as " + this.user.username, "Success"),
+      (res : string) => [
+        this.store.dispatch(isLoggedInSuccesfully()),
+        this.toast.success("Succesfully Logged in as " + this.user.username, "Success")
+      ],
       (err : HttpErrorResponse)=> this.toast.error("Could not log in.", "Error")
     );   
   }
