428a4119b59f11bb49ecff6c1b3153c103440919
max <max.maxkot@gmail.com>
2019-11-06 20:11:13 -0500

    added fade animation angular

--- angular/src/app/app.component.ts
+++ angular/src/app/app.component.ts
@@ -2,11 +2,13 @@ import { Component } from '@angular/core';
 import { Observable } from 'rxjs';
 import { LoginService } from './services/login.service';
 import { ToastService } from 'ng-uikit-pro-standard';
+import { fadeAnimation } from './animations/fade.animation';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
+  animations: [ fadeAnimation ]
 })
 export class AppComponent {
 
