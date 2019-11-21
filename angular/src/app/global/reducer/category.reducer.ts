db94b51cce639a111d7902fa51905d484357782b
max <max.maxkot@gmail.com>
2019-11-20 22:08:27 -0500

    sorts categories alphabetically

--- angular/src/app/global/reducer/category.reducer.ts
+++ angular/src/app/global/reducer/category.reducer.ts
@@ -12,15 +12,30 @@ interface State{
    
   const _categoryReducer = createReducer(
     initialState,
-    on(getCategoriesSuccesfully, (state,action) => {return {...state, categories: action.payload }}),
-    on(addCategorySuccesfully, (state, action) => {return {...state, categories: [...state.categories, action.payload]}}),
+    on(getCategoriesSuccesfully, (state,action) => getCategory(state, action)),
+    on(addCategorySuccesfully, (state, action) => addCategory(state, action)),
     on(deleteCategorySuccesfully, (state, action) => {return {...state, categories: state.categories.filter((c : Category) => c.id != action.payload.id) }}),
     on(categoryChanged, (state, action) => updateCategory(state, action))
   );
-  
+
+  function getCategory(state, action){
+    let finalState = {...state, categories: action.payload };
+    return sortCategoriesAlphabetically(finalState);
+  }
+
+  function  addCategory(state, action){
+    let finalState = {...state, categories: [...state.categories, action.payload]};
+    return sortCategoriesAlphabetically(finalState);
+  }
+
   function updateCategory(state, action){
     let index = state.categories.findIndex(x => x.id == action.payload.id)
-    return { ...state, categories: [...state.categories.splice(0, index), action.payload, ...state.categories.splice(index + 1)]};      
+    let finalState = { ...state, categories: [...state.categories.splice(0, index), action.payload, ...state.categories.splice(index + 1)]};
+    return sortCategoriesAlphabetically(finalState);     
+  }
+
+  function sortCategoriesAlphabetically(state){
+    return {...state, categories: state.categories.sort((a,b) => (a.title > b.title) ? 1 : -1)}
   }
 
   export function CategoryReducer(state, action) {
